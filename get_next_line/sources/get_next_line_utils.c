53931503d4007c57f87299a27c6c76dfc0b9aad8
EvilGrenka <evilgrenka@EvilGrenkas-MacBook-Air.local>
2022-01-26 13:36:46 +0300

    add gnl

---
+++ get_next_line/sources/get_next_line_utils.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnoriko <rnoriko@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 13:17:54 by rnoriko           #+#    #+#             */
+/*   Updated: 2022/01/26 13:30:23 by rnoriko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strndup(const char *s, size_t n)
+{
+	size_t	i;
+	char	*result;
+
+	result = (char *)malloc(sizeof(char) * (n + 1));
+	if (result == NULL)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		result[i] = s[i];
+		i++;
+	}
+	result[i] = 0;
+	return (result);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != c)
+	{
+		if (*s == '\0')
+			return (0);
+		s++;
+	}
+	return ((char *)s);
+}
+
+size_t 	ft_strlen(const char *str)
+{
+	const char *begin;
+
+	begin = str;
+	while(*str)
+		str++;
+	return (str - begin);
+}
+
+char	*ft_strjoin_free(char const *s1, char const *s2, int check)
+{
+	char	*result;
+
+	result = ft_strjoin(s1, s2);
+	if (check & 1)
+		free((char *)s1);
+	if (check & 2)
+		free((char *)s2);
+	return (result);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *) malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[j])
+		str[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (str);
+}
